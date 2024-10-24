396fcb5e337d14748a1d23b2dac7efc8806e3c94
yachu-27 <syps109074@syps.tp.edu.tw>
2024-10-24 20:57:23 +0800

    Create ex2-4.cpp

---
+++ ex2-4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+   int a =10;
+   char c = 'h';
+   float pi =3.14159;
+   
+    printf("a = %d \n",a+a +10);
+    printf("c = %c \n",c);
+    printf("pi = %f \n",pi);
+
+    return 0;
+}
