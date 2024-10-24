b610b44284cc3b600c3b65759fe6c4475eb4ca27
yachu-27 <syps109074@syps.tp.edu.tw>
2024-10-24 20:38:06 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+    printf("1Hello World\n4:This sentence will work!\n\t5:This sentence will work!\n");
+    printf("2:Tish sentence will work!\n")
+    printf("3:Tish sentence will work!\n")3\\This sentence will work!\n3:\"Tish sentence will work!\n")
+    printf("4:Tish sentence will work!\n")   
+    printf("5:Tish sentence will work!\n")
+    
+    return 0;
+}
