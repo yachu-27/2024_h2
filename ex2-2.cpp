35b03f55310cfbe3b125b3c66ef74a676d173424
yachu-27 <syps109074@syps.tp.edu.tw>
2024-10-24 19:53:15 +0800

    Create ex2-2.cpp

---
+++ ex2-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+    printf("1Hello World\n";
+    printf("2:Tish sentence will work!\n")
+    printf("3:Tish sentence will work!\n")
+    printf("4:Tish sentence will work!\n")
+    printf("5:Tish sentence will work!\n")
+    
+    return 0;
+}
