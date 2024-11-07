82715a0f9e12b707a76a71de1d5554d0d3722a43
yachu-27 <syps109074@syps.tp.edu.tw>
2024-11-07 19:51:55 +0800

    Update ex3-1.cpp

--- ex3-1.cpp
+++ ex3-1.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int i;
+    int= 1;
     float f;
     char  c;
+    int x ,y;
+    int sum;
     
-    i = 10;
+    i = 10 + i;
     f = 0.123456;
     c = 'h'
-    
+
     printf("l = %d\n",i);
     printf("f = %f\n",f);
     printf("c = %c\n",c);
