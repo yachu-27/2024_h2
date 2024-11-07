e68a66d802bcaf4d316da55104d9f89a8de3fd83
yachu-27 <syps109074@syps.tp.edu.tw>
2024-11-07 19:36:39 +0800

    Create ex3-1.cpp

---
+++ ex3-1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    float f;
+    char  c;
+    
+    i = 10;
+    f = 0.123456;
+    c = 'h'
+    
+    printf("l = %d\n",i);
+    printf("f = %f\n",f);
+    printf("c = %c\n",c);
+    return 0;
+}
